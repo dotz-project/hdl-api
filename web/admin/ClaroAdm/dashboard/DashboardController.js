1319e97c7d6bed1da0e298df6ace0bb2aaaad41e
Marcos Borges <marcos.borges@dotzmkt.com.br@localhost.localdomain>
2018-05-17 19:21:32 -0300

    First Commit

--- web/admin/ClaroAdm/dashboard/DashboardController.js
+++
@@ -1,9 +0,0 @@
-angular.module("ClaroAdm").controller("DashboardController", function ($scope, $rootScope, YII2Request, $state, $compile){
-    $scope.init = function() {
-        
-    };
-    $scope.methods = {
-        
-    };
-    $scope.init();
-})
--- web/painel/ClaroAdm/forgotPassword/ForgotPasswordController.js
+++
@@ -1,3 +0,0 @@
-angular.module("ClaroAdm").controller("ForgotPasswordController", function ($scope, YII2Request, $rootScope,$state){
-    
-})
